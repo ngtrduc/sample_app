fe627581a16f229b57f2ef9ef67afd3312992c9a
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-05-20 15:02:28 +0700

    finsh user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(param[:id])
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		#dieu khien luong neu save user thanh cong
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+  def user_params
+	params.require(:user).permit(:user_name,:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :user_name, presence: true, length: {minimum: 6}, uniqueness: {case_sensitive: false}
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, length: { maximum: 255 }, format: {with: EMAIL_FORMAT_REGEX}, uniqueness: {case_sensitive: false}
 	has_secure_password
