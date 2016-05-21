4c1bd1e4287b2ca85606f24101ce5728d17e167a
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-05-21 12:09:04 +0700

    finish login/logout

--- db/migrate/20160520024921_create_users.rb
+++ db/migrate/20160520024921_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :avatar
       t.string :user_name
       t.string :password_digest
+      t.string :remember_digest
       
       t.timestamps null: false
     end
