7f4c4985eb17ecaa9c7d5f67a89da29b449999b0
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-05-26 19:15:14 +0700

    account activation & password reset

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,14 @@ ActiveRecord::Schema.define(version: 20160520024921) do
     t.string   "user_name"
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",           default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",             default: false
+    t.string   "activation_digest"
+    t.boolean  "activated",         default: false
+    t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
