2cad0988420549de8f278bf6a1f2d99fa9dbf18d
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2016-05-27 16:36:42 +0700

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
 	attr_accessor :remember_token, :activation_token, :reset_token
 
 	has_many :microposts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy											
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+	has_many :followers, through: :passive_relationships, source: :follower
+	has_many :following, through: :active_relationships, source: :followed
 	before_save { self.email = email.downcase }
 	before_create :create_activation_digest
 	EMAIL_FORMAT_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -32,7 +36,20 @@ class User < ActiveRecord::Base
 		BCrypt::Password.new(digest).is_password?(token)
 	end	
 	def feed
-		Micropost.where("user_id = ?", id).order("created_at DESC")
+		Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+	end
+
+	# Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+	# Unfollows a user.
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+	# Returns true if the current user is following the other user.
+	def following?(other_user)
+		following.include?(other_user)
 	end
 	def forget
 		update_attributes(remember_digest: nil)
